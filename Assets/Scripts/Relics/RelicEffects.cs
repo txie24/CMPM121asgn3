e7615e1bc075e1d5daf90bbcf9b0ee2f93b94906
txie24 <167393449+txie24@users.noreply.github.com>
2025-05-26 16:11:52 -0700

    Relics Working?

--- Assets/Scripts/Relics/RelicEffects.cs
+++ Assets/Scripts/Relics/RelicEffects.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -12,30 +12,61 @@ public static class RelicEffects
 {
     public static IRelicEffect Create(EffectData d, Relic r) => d.type switch
     {
-        "gain-mana" => new GainMana(int.Parse(d.amount)),
-        "gain-spellpower" => new GainSpellPower(d.amount),
+        "gain-mana" => new GainMana(int.Parse(d.amount), r.Name),
+        "gain-spellpower" => new GainSpellPower(d.amount, r.Name),
         _ => throw new Exception($"Unknown effect {d.type}")
     };
 
     class GainMana : IRelicEffect
     {
         readonly int amt;
-        public GainMana(int a) { amt = a; }
+        readonly string relicName;
+
+        public GainMana(int a, string name)
+        {
+            amt = a;
+            relicName = name;
+        }
+
         public void Activate()
-            => GameManager.Instance.player.GetComponent<PlayerController>().GainMana(amt);
+        {
+            var pc = GameManager.Instance.player
+                          .GetComponent<PlayerController>();
+            pc.GainMana(amt);
+            // debug log so you can see the change
+            Debug.Log($"[Relic] '{relicName}' → +{amt} mana. Now {pc.spellcaster.mana}/{pc.spellcaster.max_mana} mana.");
+        }
+
         public void Deactivate() { }
     }
 
     class GainSpellPower : IRelicEffect
     {
         readonly string formula;
-        public GainSpellPower(string f) { formula = f; }
+        readonly string relicName;
+
+        public GainSpellPower(string f, string name)
+        {
+            formula = f;
+            relicName = name;
+        }
+
         public void Activate()
         {
             var vars = new Dictionary<string, int> { { "wave", GameManager.Instance.wavesCompleted } };
             int v = RPNEvaluator.Evaluate(formula, vars);
-            GameManager.Instance.player.GetComponent<PlayerController>().AddSpellPower(v);
+
+            var pc = GameManager.Instance.player
+                          .GetComponent<PlayerController>();
+            pc.AddSpellPower(v);
+
+            // debug log so you can see both the RPN result and the new total
+            Debug.Log($"[Relic] '{relicName}' → +{v} spell power (formula '{formula}'). Now {pc.spellcaster.spellPower} SP.");
         }
-        public void Deactivate() { }
+
+        public void Deactivate()
+        {
+            Debug.Log($"[Relic] '{relicName}' effect ended.");
+        }
     }
 }
